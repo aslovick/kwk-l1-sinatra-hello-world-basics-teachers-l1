88d4b71bd70f58b1e74366fd17a1b2aba8658551
Abigail Slovick <abby.slovick@gmail.com>
2018-07-31 14:02:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
-  "Hello, World!"
+  "Hello, World! My name is Abby, and I am 24 years old. I was born and raised in Kansas City before moving to Los Angeles. I am currently teaching with Kode With Klossy"
   end 
 end
